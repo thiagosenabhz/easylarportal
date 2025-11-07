832cfe4755427f1ab7ca219aa5e50dd27c60e47f
thiagosenabhz <thiagosenabhz@gmail.com>
2025-11-07 15:12:00 -0300

    Create ScrollHint.tsx

---
+++ app/components/ScrollHint.tsx
@@ -0,0 +1,9 @@
+export default function ScrollHint(){
+  return (
+    <div className="w-full flex justify-center">
+      <div className="mt-4 animate-bounce text-white/90 bg-white/10 rounded-full px-3 py-1">
+        ↓ role a página para ver mais
+      </div>
+    </div>
+  );
+}
