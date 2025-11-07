c7735920854e0c56cf79553a8b8fa546f4187605
thiagosenabhz <thiagosenabhz@gmail.com>
2025-11-07 12:45:04 -0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {
+      colors: { brandBlue: '#2E6FB3', brandGreen: '#2BB673' }
+    }
+  },
+  plugins: []
+}
+export default config
