95f987bf83ef4dc8285c7fec47e5e558eacf9c22
thiagosenabhz <thiagosenabhz@gmail.com>
2025-11-21 09:49:04 -0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,8 +2,8 @@
 
 import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.https://zepngrslqqdtmvsraawg.supabase.co;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplcG5ncnNscXFkdG12c3JhYXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMTU1NzEsImV4cCI6MjA3NTU5MTU3MX0.3lWBnHhYxAMqyPv_liMSyWKPXXKZh3gop4jQJjGHKrM;
 
 // Criamos um client único, reutilizado em toda a aplicação.
 let supabaseClient: SupabaseClient | null = null;
